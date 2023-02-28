2b63f27a32e02b228740391846457486de3d41a0
FedeDFR <federicodiforte@hotmail.com>
2023-02-28 16:22:56 -0300

    Create PruebaDeMemoria.java
    Co-Authored-By: elgeorghe <126195938+elgeorghe@users.noreply.github.com>

---
+++ ola mundo/src/PruebaDeMemoria.java
@@ -0,0 +1,17 @@
+
+public class PruebaDeMemoria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			int suma = 10 + 20;
+			
+			System.out.println(suma);
+			
+			int sumaa = suma + 2;
+			
+			System.out.println(suma)888888888888
+	}
+
+}
