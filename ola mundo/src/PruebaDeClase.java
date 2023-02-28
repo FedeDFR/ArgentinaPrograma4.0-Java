7dec00a44ae88868554e93d38904cfd312961f6a
FedeDFR <federicodiforte@hotmail.com>
2023-02-28 16:19:36 -0300

    Create PruebaDeClase.java
    Co-Authored-By: elgeorghe <126195938+elgeorghe@users.noreply.github.com>

---
+++ ola mundo/src/PruebaDeClase.java
@@ -0,0 +1,17 @@
+
+public class PruebaDeClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		char unaLetra = 'a';
+		boolean unValorBooleano = true;
+		int miPrimerContador = 66;
+		double unValor = 1.67;
+		float otroNum = 2.3434f;
+
+		System.out.println(otroNum);
+	}
+
+}
